2fe0eacea6aaf0f36d88009759ad70c200999cdc
LDVP5049 <luismaster752@gmail.com>
2024-02-14 07:43:49 -0700

    Agregar archivos de proyecto.

---
+++ StoreConePantallas/StoreConePantallas/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StoreConePantallas.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StoreConePantallas.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
