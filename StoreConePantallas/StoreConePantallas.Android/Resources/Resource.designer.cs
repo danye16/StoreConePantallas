836cfa2f73f36bd5a8ac5b4df33ee38982c6a73b
LDVP5049 <luismaster752@gmail.com>
2024-02-14 09:12:49 -0700

    1.0

--- StoreConePantallas/StoreConePantallas.Android/Resources/Resource.designer.cs
+++ StoreConePantallas/StoreConePantallas.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace StoreConePantallas.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.2.120")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.99")]
 	public partial class Resource
 	{
 		
