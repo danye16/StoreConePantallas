14016790928d6bf83fa8e8988fb10a515feda1b9
PC <danyel.16@gmail.com>
2024-02-16 00:07:46 -0600

    cambios

--- StoreConePantallas/StoreConePantallas/AssemblyInfo.cs
+++ StoreConePantallas/StoreConePantallas/AssemblyInfo.cs
@@ -1,3 +1,12 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Childzone_Regular.otf", Alias ="kawaii")]
+
+[assembly: ExportFont("TheLostDungeon.ttf", Alias = "kawaii2")]
+
+[assembly: ExportFont("Scratches.otf", Alias = "kawaii3")]
+
+[assembly: ExportFont("NoClubLoneWolf.ttf", Alias = "kawaii4")]
